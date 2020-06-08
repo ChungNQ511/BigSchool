b43acf580d1ce68d9a84c8616bbf17c833296174
Administrator <Administrator@PC208>
2020-06-08 14:43:35 +0700

    Buoithu2

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -16,6 +16,7 @@ namespace BigSchool.Controllers
         {
             _dbContext = new ApplicationDbContext();
         }
+        [Authorize]
         // GET: Courses
         public ActionResult Create()
         {
@@ -27,6 +28,7 @@ namespace BigSchool.Controllers
         }
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
             if(!ModelState.IsValid)
